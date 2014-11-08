63d656cd8b29512338d6622365f66d0dfb9d06b3
Alexey Kotlyarov <a@koterpillar.com>
2014-11-08 20:51:07 +1100

    Index and article routes

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,8 @@
+{-# Language FlexibleContexts #-}
 module Main where
 
 import Control.Monad
+import Control.Monad.State
 
 import Data.Time
 
@@ -8,25 +10,92 @@ import Happstack.Server
 
 
 main :: IO ()
-main = simpleHTTP nullConf $ msum [ mzero
-                                  , index
-                                  , article
-                                  ]
+main = simpleHTTP' runApp nullConf $ msum [ mzero
+                                          , index
+                                          , yearlyIndex
+                                          , monthlyIndex
+                                          , dailyIndex
+                                          , article
+                                          ]
 
 data Article = Article { arTitle :: String
                        , arContent :: Content
-                       , arAuthored :: UniversalTime
+                       , arAuthored :: UTCTime
                        }
 
 type Content = String
 
-index :: ServerPartT IO Response
-index = articleList $ const True
+data AppState = AppState { appArticles :: [Article]
+                         }
 
--- TODO: Monthly, daily indices
+getFiltered :: MonadState AppState m => (Article -> Bool) -> m [Article]
+getFiltered articleFilter = gets $ filter articleFilter . appArticles
 
-articleList :: (Article -> Bool) -> ServerPartT IO Response
-articleList = undefined
+getOne :: (MonadState AppState m, MonadPlus m) =>
+    (Article -> Bool) -> m Article
+getOne articleFilter = do
+    articles <- getFiltered articleFilter
+    case articles of
+        [article] -> return article
+        _ -> mzero
+
+byDate :: Day -> Article -> Bool
+byDate d = (== d) . utctDay . arAuthored
+
+byYear :: Integer -> Article -> Bool
+byYear y a = y == y'
+    where (y', _, _) = toGregorian $ utctDay $ arAuthored a
+
+byYearMonth :: Integer -> Int -> Article -> Bool
+byYearMonth y m a = y == y' && m == m'
+    where (y', m', _) = toGregorian $ utctDay $ arAuthored a
+
+bySlug :: String -> Article -> Bool
+bySlug slug = (== slug) . slugify . arTitle
+
+byDateSlug :: Day -> String -> Article -> Bool
+byDateSlug d s a = byDate d a && bySlug s a
+
+-- TODO
+slugify :: String -> String
+slugify = id
+
+type App = StateT AppState IO
+
+runApp :: App a -> IO a
+runApp a = evalStateT a initialState
+    where initialState = AppState [ someArticle
+                                  ]
+          someArticle = Article "test" "test article" someDate
+          someDate = UTCTime (fromGregorian 2014 11 08) 0
+
+index :: ServerPartT App Response
+index = do
+    nullDir
+    articleList $ const True
+
+yearlyIndex :: ServerPartT App Response
+yearlyIndex = path $ \year -> do
+    nullDir
+    articleList $ byYear year
+
+monthlyIndex :: ServerPartT App Response
+monthlyIndex = path $ \year -> path $ \ month -> do
+    nullDir
+    articleList $ byYearMonth year month
+
+dailyIndex :: ServerPartT App Response
+dailyIndex = dayPath $ \date -> do
+    nullDir
+    articleList $ byDate date
+
+articleList :: (Article -> Bool) -> ServerPartT App Response
+articleList articleFilter = do
+    articles <- getFiltered articleFilter
+    articleListDisplay articles
+
+articleListDisplay :: [Article] -> ServerPartT App Response
+articleListDisplay = ok . toResponse . show . map arContent
 
 dayPath :: (ServerMonad m, MonadPlus m) => (Day -> m b) -> m b
 dayPath handler =
@@ -36,10 +105,11 @@ dayPath handler =
         Nothing -> mzero
         Just date -> handler date
 
-article :: ServerPartT IO Response
+article :: ServerPartT App Response
 article = dayPath $ \date -> path $ \slug -> do
     nullDir
-    ok $ toResponse (slug :: String)
+    article <- getOne $ byDateSlug date slug
+    articleDisplay article
 
-articleDisplay :: Article -> ServerPartT IO Response
-articleDisplay = undefined
+articleDisplay :: Article -> ServerPartT App Response
+articleDisplay = ok . toResponse . arContent
