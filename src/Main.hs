2eb74fabee90242bc7748cdf83ecf622744872f2
Alexey Kotlyarov <a@koterpillar.com>
2014-11-08 22:17:10 +1100

    Match on multiple path segments

--- src/Main.hs
+++ src/Main.hs
@@ -21,28 +21,49 @@ main = simpleHTTP' runApp nullConf $ msum [ mzero
                                           , article
                                           ]
 
+-- A route that matches on multiple path segments
+class Route a where
+    route :: (ServerMonad m, MonadPlus m) => (a -> m b) -> m b
+
+newtype Path p = Path p
+
+instance FromReqURI a => Route (Path a) where
+    route h = path $ h . Path
+
+instance (Route a, Route b) => Route (a, b) where
+    route handler = route $ \a -> route $ \b -> handler (a, b)
+
+instance (Route a, Route b, Route c) => Route (a, b, c) where
+    route handler = route $ \a -> route $ \b -> route $ \c -> handler (a, b, c)
+
+instance Route Day where
+    route handler = route $ \(Path y, Path m, Path d) ->
+        case fromGregorianValid y m d of
+            Nothing -> mzero
+            Just date -> handler date
+
 index :: ServerPartT App Response
 index = do
     nullDir
     articleList $ const True
 
 yearlyIndex :: ServerPartT App Response
-yearlyIndex = path $ \year -> do
+yearlyIndex = route $ \(Path year) -> do
     nullDir
     articleList $ byYear year
 
 monthlyIndex :: ServerPartT App Response
-monthlyIndex = path $ \year -> path $ \ month -> do
+monthlyIndex = route $ \(Path year, Path month) -> do
     nullDir
     articleList $ byYearMonth year month
 
 dailyIndex :: ServerPartT App Response
-dailyIndex = dayPath $ \date -> do
+dailyIndex = route $ \date -> do
     nullDir
     articleList $ byDate date
 
 article :: ServerPartT App Response
-article = dayPath $ \date -> path $ \slug -> do
+article = route $ \(date , Path slug) -> do
     nullDir
     article <- getOne $ byDateSlug date slug
     articleDisplay article
@@ -51,11 +72,3 @@ articleList :: (Article -> Bool) -> ServerPartT App Response
 articleList articleFilter = do
     articles <- getFiltered articleFilter
     articleListDisplay articles
-
-dayPath :: (ServerMonad m, MonadPlus m) => (Day -> m b) -> m b
-dayPath handler =
-    path $ \year ->
-    path $ \month ->
-    path $ \day -> case fromGregorianValid year month day of
-        Nothing -> mzero
-        Just date -> handler date
