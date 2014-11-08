50c3c07faa2a15e91c612b7d02b67abe1cdd1675
Alexey Kotlyarov <a@koterpillar.com>
2014-11-08 17:10:34 +1100

    Article router

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,45 @@
 module Main where
 
-import Happstack.Server (nullConf, simpleHTTP, toResponse, ok)
+import Control.Monad
+
+import Data.Time
+
+import Happstack.Server
+
 
 main :: IO ()
-main = simpleHTTP nullConf $ ok "Hello, World"
+main = simpleHTTP nullConf $ msum [ mzero
+                                  , index
+                                  , article
+                                  ]
+
+data Article = Article { arTitle :: String
+                       , arContent :: Content
+                       , arAuthored :: UniversalTime
+                       }
+
+type Content = String
+
+index :: ServerPartT IO Response
+index = articleList $ const True
+
+-- TODO: Monthly, daily indices
+
+articleList :: (Article -> Bool) -> ServerPartT IO Response
+articleList = undefined
+
+dayPath :: (ServerMonad m, MonadPlus m) => (Day -> m b) -> m b
+dayPath handler =
+    path $ \year ->
+    path $ \month ->
+    path $ \day -> case fromGregorianValid year month day of
+        Nothing -> mzero
+        Just date -> handler date
+
+article :: ServerPartT IO Response
+article = dayPath $ \date -> path $ \slug -> do
+    nullDir
+    ok $ toResponse (slug :: String)
+
+articleDisplay :: Article -> ServerPartT IO Response
+articleDisplay = undefined
