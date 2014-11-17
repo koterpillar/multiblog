702e88c1250dd2a18a8e9db7233f883aef3aae01
Alexey Kotlyarov <a@koterpillar.com>
2014-11-17 17:12:57 +1100

    Hamlet in separate files

--- src/Views.hs
+++ src/Views.hs
@@ -1,4 +1,4 @@
-{-# Language QuasiQuotes #-}
+{-# Language TemplateHaskell #-}
 module Views where
 
 import Control.Monad
@@ -12,7 +12,7 @@ import Happstack.Server
 
 import Text.Blaze.Html (Markup)
 import Text.Blaze.Html.Renderer.String (renderHtml)
-import Text.Hamlet (shamlet)
+import Text.Hamlet (shamletFile)
 import Text.Pandoc
 
 import App
@@ -29,23 +29,12 @@ page :: String -> Markup -> PageContent
 page title content = defaultPage { pcTitle = title, pcContent = content }
 
 template :: PageContent -> ServerPartT App Response
-template page = ok $ toResponse $ [shamlet|
-    <html>
-        <head>
-            <title>#{pcTitle page}
-        <body>
-            <h1>#{pcTitle page}
-            #{pcContent page}
-|]
+template page = ok $ toResponse $ $(shamletFile "templates/base.hamlet")
 
 articleListDisplay :: [Article] -> ServerPartT App Response
-articleListDisplay articles = template $ page "List" [shamlet|
-    $forall article <- articles
-        <h2>#{arTitle article}
-        <p>#{writeHtml def $ arContent article}
-|]
+articleListDisplay articles = template $
+    page "List" $(shamletFile "templates/list.hamlet")
 
 articleDisplay :: Article -> ServerPartT App Response
-articleDisplay article = template $ page (arTitle article) [shamlet|
-    <p>#{writeHtml def $ arContent article}
-|]
+articleDisplay article = template $
+    page (arTitle article) $(shamletFile "templates/article.hamlet")
