91caaa6dde668adbf37be020da64eeaa80312c3c
Alexey Kotlyarov <a@koterpillar.com>
2015-01-19 20:20:13 +1100

    Clean up Main

--- src/App.hs
+++ src/App.hs
@@ -2,11 +2,26 @@ module App where
 
 import Control.Monad.State
 
+import qualified Data.ByteString.Char8 as B
+import Data.Time
+
+import Happstack.Server
+
+import Web.Routes
+import Web.Routes.Boomerang
+import Web.Routes.Happstack ()
+
 import Import
+import Language
 import Models
+import Routes
+import Utils
+import Views
 
 
 type App = StateT AppState IO
+
+type AppPart a = RouteT Sitemap (ServerPartT App) a
 
 runApp :: App a -> IO a
 runApp a = do
@@ -16,3 +31,54 @@ runApp a = do
         Right appState -> do
             print appState
             evalStateT a appState
+
+site :: Site Sitemap (ServerPartT App Response)
+site = boomerangSiteRouteT handler sitemap
+
+handler :: Sitemap -> AppPart Response
+handler route = case route of
+    Index -> index
+    Yearly y -> yearlyIndex y
+    Monthly y m -> monthlyIndex y m
+    Daily d -> dailyIndex d
+    ArticleView d s -> article d s
+    MetaView s -> meta s
+
+index :: AppPart Response
+index = articleList $ const True
+
+yearlyIndex :: Integer -> AppPart Response
+yearlyIndex = articleList . byYear
+
+monthlyIndex :: Integer -> Int -> AppPart Response
+monthlyIndex year month = articleList $ byYearMonth year month
+
+dailyIndex :: Day -> AppPart Response
+dailyIndex date = articleList $ byDate date
+
+languageHeaderM :: AppPart LanguagePreference
+languageHeaderM = do
+    request <- askRq
+    let header = getHeader "Accept-Language" request
+    return $ languageHeader $ liftM B.unpack header
+
+html :: ToMessage a => a -> AppPart Response
+html = ok . toResponse
+
+article :: Day -> String -> AppPart Response
+article date slug = do
+    language <- languageHeaderM
+    a <- onlyOne $ lift $ getFiltered $ byDateSlug date slug
+    articleDisplay language a >>= html
+
+articleList :: (Article -> Bool) -> AppPart Response
+articleList articleFilter = do
+    articles <- lift $ getFiltered articleFilter
+    language <- languageHeaderM
+    articleListDisplay language articles >>= html
+
+meta :: String -> AppPart Response
+meta slug = do
+    language <- languageHeaderM
+    m <- getMeta slug
+    metaDisplay language m >>= html
--- src/Main.hs
+++ src/Main.hs
@@ -2,42 +2,16 @@
 {-# LANGUAGE TypeOperators     #-}
 module Main where
 
-import Control.Monad
-import Control.Monad.State
-
-import qualified Data.ByteString.Char8 as B
 import Data.Maybe
 import qualified Data.Text as T
-import Data.Time
 
 import Happstack.Server
 
 import System.Environment
 
-import Web.Routes
-import Web.Routes.Boomerang
 import Web.Routes.Happstack
 
 import App
-import Language
-import Models
-import Routes
-import Utils
-import Views
-
-type AppPart a = RouteT Sitemap (ServerPartT App) a
-
-handler :: Sitemap -> AppPart Response
-handler route = case route of
-    Index -> index
-    Yearly y -> yearlyIndex y
-    Monthly y m -> monthlyIndex y m
-    Daily d -> dailyIndex d
-    ArticleView d s -> article d s
-    MetaView s -> meta s
-
-site :: Site Sitemap (ServerPartT App Response)
-site = boomerangSiteRouteT handler sitemap
 
 siteAddress :: IO String
 siteAddress = do
@@ -50,43 +24,3 @@ main = do
     listenPort <- lookupEnv "LISTEN_PORT"
     let conf = nullConf { port = read $ fromMaybe "8000" listenPort }
     simpleHTTP' runApp conf $ implSite (T.pack address) "" site
-
-index :: AppPart Response
-index = articleList $ const True
-
-yearlyIndex :: Integer -> AppPart Response
-yearlyIndex = articleList . byYear
-
-monthlyIndex :: Integer -> Int -> AppPart Response
-monthlyIndex year month = articleList $ byYearMonth year month
-
-dailyIndex :: Day -> AppPart Response
-dailyIndex date = articleList $ byDate date
-
-languageHeaderM :: AppPart LanguagePreference
-languageHeaderM = do
-    request <- askRq
-    let header = getHeader "Accept-Language" request
-    return $ languageHeader $ liftM B.unpack header
-
-html :: ToMessage a => a -> AppPart Response
-html = ok . toResponse
-
-article :: Day -> String -> AppPart Response
-article date slug = do
-    language <- languageHeaderM
-    -- TODO: onlyOne
-    a <- onlyOne $ lift $ getFiltered $ byDateSlug date slug
-    articleDisplay language a >>= html
-
-articleList :: (Article -> Bool) -> AppPart Response
-articleList articleFilter = do
-    articles <- lift $ getFiltered articleFilter
-    language <- languageHeaderM
-    articleListDisplay language articles >>= html
-
-meta :: String -> AppPart Response
-meta slug = do
-    language <- languageHeaderM
-    m <- getMeta slug
-    metaDisplay language m >>= html
