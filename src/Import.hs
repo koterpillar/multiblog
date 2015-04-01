e628430c6a7d32c6af36c53de4f4c2a2226a38f3
Alexey Kotlyarov <a@koterpillar.com>
2015-04-01 22:06:42 +1100

    Remove strings from which everything has been extracted

--- src/Import.hs
+++ src/Import.hs
@@ -89,38 +89,51 @@ extractContent cs = flip evalStateT (metaValues cs) $ do
 -- MetaInfo sets can be used to extract typed information
 -- Extracting something changes the set
 
+-- Extracting a value from a source consumes all or part of the source
+type Extractor source value = source -> Maybe (value, Maybe source)
+
+liftExtracted :: (source -> source') -> Maybe (value, Maybe source) -> Maybe (value, Maybe source')
+liftExtracted = liftM . second . liftM
+
 -- Try to read the language off the end of a string
 extractLanguage :: MonadState [MetaInfo] s => s (Maybe Language)
 extractLanguage = extractFromMeta "language" parseLang
     where split3 :: String -> (String, String)
           split3 (x:y:z:t:u) = ((x:hd), tl) where (hd, tl) = split3 (y:z:t:u)
           split3 tl = ("", tl)
-          parseLang :: String -> Maybe (Language, String)
+          parseLang :: Extractor String Language
           parseLang str = let (hd, tl) = split3 str in case tl of
               '-':langStr -> case parseLanguage langStr of
-                                 Just lang -> Just (lang, hd)
+                                 Just lang -> Just (lang, notEmpty hd)
                                  _ -> Nothing
-              _ -> Nothing
+              langStr -> case parseLanguage langStr of
+                             Just lang -> Just (lang, Nothing)
+                             _ -> Nothing
 
 -- Try to read the date off the start
 extractDate :: MonadState [MetaInfo] s => s (Maybe UTCTime)
 extractDate = extractFromMeta "date" $ \str -> case reads str of
-    [(dt, rest)] -> Just (atMidnight dt, rest)
+    [(dt, rest)] -> Just (atMidnight dt, notEmpty rest)
     _ -> Nothing
+
+-- Treat empty lists/strings as Nothing
+notEmpty :: [a] -> Maybe [a]
+notEmpty [] = Nothing
+notEmpty s = Just s
 
 -- Extract the slug, this can be any string
 extractSlug :: MonadState [MetaInfo] s => s (Maybe String)
-extractSlug = extractFromMeta "slug" $ \str -> Just (str, "")
+extractSlug = extractFromMeta "slug" $ \str -> Just (str, Nothing)
 
 -- Extract either unnamed or named meta, applying a parser afterwards
-extractFromMeta :: MonadState [MetaInfo] s => String -> (String -> Maybe (a, String)) -> s (Maybe a)
+extractFromMeta :: MonadState [MetaInfo] s => String -> Extractor String a -> s (Maybe a)
 extractFromMeta name parser = extractIf test
-    where test (Named name' value) | name == name' = liftM (second (Named name)) $ parser value
+    where test (Named name' value) | name == name' = liftExtracted (Named name) $ parser value
                                    | otherwise = Nothing
-          test (Unnamed value) = liftM (second Unnamed) $ parser value
+          test (Unnamed value) = liftExtracted Unnamed $ parser value
 
 -- Extract a meta matching a function
-extractIf :: MonadState [c] s => (c -> Maybe (a, c)) -> s (Maybe a)
+extractIf :: MonadState [c] s => Extractor c a -> s (Maybe a)
 extractIf test = do
         st <- get
         let (result, st') = tryExtract st
@@ -128,7 +141,8 @@ extractIf test = do
         return result
     where tryExtract [] = (Nothing, [])
           tryExtract (m:ms) = case test m of
-              Just (result, m') -> (Just result, m':ms)
+              Just (result, Nothing) -> (Just result, ms)
+              Just (result, Just m') -> (Just result, m':ms)
               Nothing -> (result, m:ms') where (result, ms') = tryExtract ms
 
 -- Load the application state from a directory
