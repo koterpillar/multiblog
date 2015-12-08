0cf49c14b3dcbeb90b62bab5e60728e938c6ea24
Alexey Kotlyarov <a@koterpillar.com>
2015-12-08 21:35:31 +1100

    Abstract away Happstack requests in tests

--- testsuite/Integration/TestHome.hs
+++ testsuite/Integration/TestHome.hs
@@ -11,23 +11,21 @@ import Test.Framework
 import Integration.Base
 
 
+homeRequest = simpleRequest "/"
+
 test_home = do
-    req <- mkRequest "/"
-    home <- testResponse req
+    home <- makeRequest $ homeRequest
     assertContains "Test site" home
     assertContainsBefore "Another article" "First test article" home
 
 test_home_lang = do
-    req <- liftM (withLang1 RU) (mkRequest "/")
-    home <- testResponse req
+    home <- makeRequest $ withLang1 RU $ homeRequest
     assertContains "Главная" home
 
 test_explicit_lang = do
-    req <- mkRequest "/?lang=ru"
-    home <- testResponse req
+    home <- makeRequest $ simpleRequest "/?lang=ru"
     assertContains "Главная" home
 
 test_cookie_lang = do
-    req <- liftM (withLangCookie ZH) (mkRequest "/")
-    home <- testResponse req
+    home <- makeRequest $ withLangCookie ZH $ homeRequest
     assertContains "首页" home
