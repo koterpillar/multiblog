bdd2a67bd99410a9912945f369a8b569e2023b22
Alexey Kotlyarov <a@koterpillar.com>
2020-02-15 21:08:59 +1100

    Fix HTF imports

--- testsuite/TestMain.hs
+++ testsuite/TestMain.hs
@@ -1,18 +1,18 @@
 {-# OPTIONS_GHC -F -pgmF htfpp #-}
 
-import           Test.Framework
+import Test.Framework
 
-import           Integration.TestArticle
-import           Integration.TestFeed
-import           Integration.TestHome
-import           Integration.TestMeta
-import           Integration.TestStatic
-import           TestCache
-import           TestImport
-import           TestLanguage
-import           TestModels
-import           TestRoutes
-import           TestViews
-import           TestViewsExport
+import {-@ HTF_TESTS @-} Integration.TestArticle
+import {-@ HTF_TESTS @-} Integration.TestFeed
+import {-@ HTF_TESTS @-} Integration.TestHome
+import {-@ HTF_TESTS @-} Integration.TestMeta
+import {-@ HTF_TESTS @-} Integration.TestStatic
+import {-@ HTF_TESTS @-} TestCache
+import {-@ HTF_TESTS @-} TestImport
+import {-@ HTF_TESTS @-} TestLanguage
+import {-@ HTF_TESTS @-} TestModels
+import {-@ HTF_TESTS @-} TestRoutes
+import {-@ HTF_TESTS @-} TestViews
+import {-@ HTF_TESTS @-} TestViewsExport
 
 main = htfMain htf_importedTests
