f18266bfe6a61a707ebeb7c97880b56832dab66b
Alexey Kotlyarov <a@koterpillar.com>
2022-01-12 21:33:15 +1100

    Fix reloading on HUP

--- src/AppMain.hs
+++ src/AppMain.hs
@@ -1,17 +1,11 @@
-{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module AppMain
     ( multiblog
     ) where
 
-import           App
 import           ReloadHup
 import           Serve
 
 multiblog :: IO ()
-multiblog =
-    reloadHup $ do
-        app <- loadAppDefault
-        cache <- initAppCache
-        runApp cache app serve
+multiblog = reloadHup serve
--- src/Serve.hs
+++ src/Serve.hs
@@ -3,11 +3,10 @@ Action to serve the blog.
 -}
 module Serve where
 
-import           Control.Exception
 import           Control.Monad.Reader
-import           Control.Monad.State
+import           Control.Monad.Trans.Resource
 
-import           Happstack.Server     hiding (bindPort)
+import           Happstack.Server             hiding (bindPort)
 
 import           Network.Socket
 
@@ -15,33 +14,34 @@ import           System.Environment
 
 import           App
 
-bindPort :: Int -> IO Socket
-bindPort portNumber = do
-    let hints =
-            defaultHints
-                { addrFlags = [AI_NUMERICHOST, AI_NUMERICSERV]
-                , addrSocketType = Stream
-                }
-    addr:_ <- getAddrInfo (Just hints) (Just "::") (Just $ show portNumber)
-    sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-    bind sock (addrAddress addr)
-    listen sock 5
-    pure sock
+bindPort :: Int -> ResourceT IO Socket
+bindPort portNumber =
+    fmap snd $
+    flip allocate close $ do
+        let hints =
+                defaultHints
+                    { addrFlags = [AI_NUMERICHOST, AI_NUMERICSERV]
+                    , addrSocketType = Stream
+                    }
+        addr:_ <- getAddrInfo (Just hints) (Just "::") (Just $ show portNumber)
+        sock <-
+            socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+        bind sock (addrAddress addr)
+        listen sock 5
+        pure sock
 
 -- Serve the site contents, handling SIGHUP
-serve :: App ()
-serve = do
-    lport <- liftIO listenPort
-    liftIO $ putStrLn $ "Serving on port " ++ show lport ++ "."
-    let conf = nullConf {port = lport}
-    app <- ask
-    cache <- get
+serve :: IO ()
+serve =
+    runResourceT $ do
+        lport <- liftIO listenPort
     -- Manually bind the socket to close it on exception
-    liftIO $
-        bracket
-            (bindPort lport)
-            close
-            (\sock -> simpleHTTPWithSocket' (runApp cache app) sock conf site)
+        sock <- bindPort lport
+        liftIO $ putStrLn $ "Serving on port " ++ show lport ++ "."
+        let conf = nullConf {port = lport}
+        cache <- liftIO initAppCache
+        app <- liftIO loadAppDefault
+        liftIO $ simpleHTTPWithSocket' (runApp cache app) sock conf site
 
 listenPort :: IO Int
 listenPort = maybe 8000 read <$> lookupEnv "PORT"
